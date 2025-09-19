fd6b6ce20a767d0dcf202154b229f5c884dcf1a4
CodeKouser <kouserbanu3@gmail.com>
2025-09-20 01:04:01 +0530

    changed process mango uri name

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { PRIVATE_MONGO_URI } from "$env/static/private";
+import { PRIVATE_MONGO_URI } from process.env.MONGO_URI;
 
 // This creates a single MongoClient instance that will be reused.
 const client = new MongoClient(PRIVATE_MONGO_URI);
