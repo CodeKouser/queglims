5111e7bf296e38ecbb6d20b151e1cb531ac1e211
CodeKouser <101180168+CodeKouser@users.noreply.github.com>
2025-09-20 01:53:16 +0530

    Update db.js

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,14 +1,12 @@
 import { MongoClient } from "mongodb";
-const uri = process.env.MONGO_URI; // This is the correct way
 
-// This creates a single MongoClient instance that will be reused.
-const client = new MongoClient(uri);
+const uri = process.env.MONGO_URI;
 
 if (!uri) {
-  throw new Error(
-    "MONGO_URI is not defined. Please set it in your environment variables."
-  );
+  throw new Error("MONGO_URI is not defined. Please set it in your environment variables.");
 }
 
-// Export the connected client instance.
+const client = new MongoClient(uri);
+
+// Export the connected client instance
 export default client.connect();
