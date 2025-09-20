139467c37ad6ac341bef0e129b791d3fd5a83f6e
CodeKouser <101180168+CodeKouser@users.noreply.github.com>
2025-09-20 18:21:51 +0530

    Update db.js

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -2,7 +2,6 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGO_URI;
 
-console.log('MONGO_URI:',.env.MONGO_URI);
 
 if (!uri) {
   throw new Error("MONGO_URI is not defined. Please set it in your environment variables.");
